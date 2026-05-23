9a60401ab57136c1098133558dd4c37db1b9f0df
Bruno Dalmazo <88686749+brunoldalmazo@users.noreply.github.com>
2026-05-23 12:46:13 -0300

    Update backup_users.js

--- backend/public/backup_users.js
+++ backend/public/backup_users.js
@@ -7,40 +7,41 @@
     - apagar este arquivo depois do deploy
 */
 const usersBackup = [
-    {
-        id: 1,
-        nome: "admin",
-        descricao: "Admin newbie",
-        saldo: 1000,
-        tipo: "admin",
-        senha: "$2b$04$ZlG3Au5TqMKJdmx5fK4ZJ.66v.l6HVy2QGwEc1i0nKbKaBN1ysbM6"
-    },
-
-    {
-        id: 2,
-        nome: "maria",
-        descricao: "Aluno",
-        saldo: 100,
-        tipo: "user",
-        senha: "$2b$04$y1KlUAvfF53f46k4lNg.xO.oywGU5jABTgQJuuwkt2TUGUmJKp4yu"
-    },
-
-    {
-        id: 3,
-        nome: "dalmazo",
-        descricao: "Admin master",
-        saldo: 100,
-        tipo: "admin",
-        senha: "$2b$04$nnmK46bQoam7Iva522vugOkdqbm/28kW2qqnpMUD/J8.NXEOZEX/O"
-    },
-
-    {
-        id: 4,
-        nome: "joao",
-        descricao: "Aluno",
-        saldo: 100,
-        tipo: "user",
-        senha: "$2b$04$d96p.qgwjTO93Zxglos88eLsweJUrdjRaHWjO43.CeHE15ScLlba6"
-    }
+    
+  {
+    id: 1,
+    nome: "admin",
+    descricao: "Admin newbie",
+    saldo: 1000,
+    tipo: "admin",
+    senha: "$2b$04$hKTgG2jEr57ypuhgmMN6bOdial8rLCbLhDDKm6KT43deA5..e1T02"
+  },
+    
+  {
+    id: 2,
+    nome: "joao",
+    descricao: "Aluno com senha fraca",
+    saldo: 100,
+    tipo: "user",
+    senha: "$2b$04$yl.N6mVNKxw4lKQ2GXyJ6.LFCx2AWEzQagHcq2FpktojaVfqRtmCW"
+  },
+    
+  {
+    id: 3,
+    nome: "dalmazo",
+    descricao: "Admin master",
+    saldo: 100,
+    tipo: "admin",
+    senha: "$2b$10$/8Fpb9ZEcHeXyhpqiDNvDuj/5euH6SdjHehVBsValkaNsJ3ceKJ26"
+  },
+    
+  {
+    id: 4,
+    nome: "maria",
+    descricao: "Aluno com senha forte",
+    saldo: 100,
+    tipo: "user",
+    senha: "$2b$10$ocuHIq7AkJLtgW2MYwuDjeLOjOeGd4tvtBPSKFIYl3aq6QL3lSR2."
+  }
     
 ];
