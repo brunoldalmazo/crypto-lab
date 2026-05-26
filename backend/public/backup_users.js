50f047dd94dacbd4063e6c4cb8a30bbe08b16db5
Bruno Dalmazo <88686749+brunoldalmazo@users.noreply.github.com>
2026-05-26 12:51:48 -0300

    Update backup_users.js

--- backend/public/backup_users.js
+++ backend/public/backup_users.js
@@ -6,42 +6,44 @@
     - revisar usuários
     - apagar este arquivo depois do deploy
 */
-const usersBackup = [
-    
-  {
-    id: 1,
-    nome: "admin",
-    descricao: "Admin newbie",
-    saldo: 1000,
-    tipo: "admin",
-    senha: "$2b$04$hKTgG2jEr57ypuhgmMN6bOdial8rLCbLhDDKm6KT43deA5..e1T02"
-  },
-    
-  {
-    id: 2,
-    nome: "joao",
-    descricao: "Aluno com senha fraca",
-    saldo: 100,
-    tipo: "user",
-    senha: "$2b$04$yl.N6mVNKxw4lKQ2GXyJ6.LFCx2AWEzQagHcq2FpktojaVfqRtmCW"
-  },
-    
-  {
-    id: 3,
-    nome: "dalmazo",
-    descricao: "Admin master",
-    saldo: 100,
-    tipo: "admin",
-    senha: "$2b$10$/8Fpb9ZEcHeXyhpqiDNvDuj/5euH6SdjHehVBsValkaNsJ3ceKJ26"
-  },
-    
-  {
-    id: 4,
-    nome: "maria",
-    descricao: "Aluno com senha forte",
-    saldo: 100,
-    tipo: "user",
-    senha: "$2b$10$ocuHIq7AkJLtgW2MYwuDjeLOjOeGd4tvtBPSKFIYl3aq6QL3lSR2."
-  }
-    
+const users = [
+    {
+        id: 1,
+        nome: "admin",
+        descricao: "Admin newbie",
+        saldo: 1000,
+        tipo: "admin",
+        senha:
+            "???" /* "Dalmazo disse que o newbie costumava guardar a senha em uma figura! */
+    },
+
+    {
+        id: 2,
+        nome: "maria",
+        descricao: "Aluno com senha forte",
+        saldo: 100,
+        tipo: "user",
+        senha:
+            "$2b$10$qCMitotrxsCsDkfNIB/Sv.VOHrLlrxqd.I2d0jPWspXIQLyTnNcLa"
+    },
+
+    {
+        id: 3,
+        nome: "joao",
+        descricao: "Aluno com senha fraca",
+        saldo: 100,
+        tipo: "user",
+        senha:
+            "1234"
+    },
+
+    {
+        id: 4,
+        nome: "dalmazo",
+        descricao: "Admin master",
+        saldo: 100,
+        tipo: "admin",
+        senha:
+            "???" /* Hash de senha com 8 caracteres se quebra em menos de 1h por força bruta... será? */
+    }
 ];
